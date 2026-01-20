44601b67235014b232605d54b9bcecf806fa34d3
brbn <kubon.kuznicki@gmail.com>
2026-01-20 15:02:17 +0100

    feat: add CORS configuration to allow requests from specified origins

--- tracker/src/main/java/com/gametracker/tracker/TrackerApplication.java
+++ tracker/src/main/java/com/gametracker/tracker/TrackerApplication.java
@@ -2,11 +2,30 @@ package com.gametracker.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer()
+	{
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "https://game-backlog-tracker-omega.vercel.app")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Authorization","Content-Type", "*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
